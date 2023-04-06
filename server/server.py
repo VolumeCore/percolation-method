be2e9226faad64af7cb7a283cd432bb67687082b
Daria-creator-lab <pochtadlyunivera@mail.ru>
2023-04-06 18:35:33 +0300

    eeeee

--- server/server.py
+++ server/server.py
@@ -1,10 +1,18 @@
-import random
-from flask import Flask
 import numpy as np
 from itertools import product
+import os
+import random
+from flask import Flask, render_template
 
-app = Flask(__name__)
+project_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+client_dir = os.path.join(project_dir, 'client')
+static_dir = os.path.join(client_dir, 'static')
+template_dir = os.path.join(client_dir, 'templates')
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 
+@app.route("/")
+def hello():
+    return render_template('index.html')
 
 @app.route("/generateMatrix/<int:size>/<int:concentration>", methods=['GET'])
 def genetation_matrix(concentration, size):
