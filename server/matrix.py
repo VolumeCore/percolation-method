90fd6b8cda22505b613e454fefb1cacd42c8d92d
SirlLizz <sleepvalera@gmail.com>
2023-04-16 13:51:29 +0400

    Add H and rain test matrix

--- server/matrix.py
+++ server/matrix.py
@@ -36,6 +36,12 @@ class Matrix:
     def horizontal_zebra_matrix(self):
         return [[(1 if j % 2 == 0 else 0) for _ in range(self.size)] for j in range(self.size)]
 
+    def vertical_rain_matrix(self):
+        return [[(random.randint(0, 1) if i % 2 == 0 else 0) for i in range(self.size)] for _ in range(self.size)]
+
+    def horizontal_rain_matrix(self):
+        return [[(random.randint(0, 1) if j % 2 == 0 else 0) for _ in range(self.size)] for j in range(self.size)]
+
     def chess_matrix(self):
         return [[((0 if j % 2 == 0 else 1) if i % 2 == 0 else (1 if j % 2 == 0 else 0)) for i in range(self.size)] for j
                 in range(self.size)]
@@ -46,6 +52,27 @@ class Matrix:
             matrix[i:self.size - i, i:self.size - i] = (1 if i % 2 == 0 else 0)
         return matrix.tolist()
 
+    def H_matrix(self):
+        block_size = 8
+        matrix = np.zeros((self.size, self.size), dtype=int)
+        for i in range(self.size//block_size):
+            for j in range(self.size // block_size):
+                matrix[2+j*block_size, i*block_size: i*block_size+5], \
+                    matrix[i * block_size: i * block_size + 5, j * block_size], \
+                    matrix[i * block_size: i * block_size + 5, j * block_size + 4] = 1, 1, 1
+        return matrix
+
+
+    def H_shift_matrix(self):
+        block_size = 8
+        matrix = np.zeros((self.size, self.size), dtype=int)
+        for i in range(self.size//block_size):
+            for j in range(self.size // block_size):
+                matrix[2+j*block_size, i*block_size + block_size//2*j: i*block_size+5 + block_size//2*j], \
+                    matrix[i * block_size: i * block_size + 5, j * block_size + i*block_size//2], \
+                    matrix[i * block_size: i * block_size + 5, j * block_size + 4 + i*block_size//2] = 1, 1, 1
+        return matrix
+
 
 if __name__ == "__main__":
-    print(np.array(Matrix(10, 12).cross_matrix()))
+    print(np.array(Matrix(10, 20).vertical_rain_matrix()))
