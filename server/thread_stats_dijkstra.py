60e25b197920c8e781c5c8ede944c6ba1db90d80
SirlLizz <sleepvalera@gmail.com>
2023-05-24 22:44:38 +0400

    Correct stats to Dijkstra

--- server/thread_stats_dijkstra.py
+++ server/thread_stats_dijkstra.py
@@ -2,13 +2,10 @@ import random
 from time import time
 import numpy as np
 import pandas as pd
-from hoshenKopelman import HoshenKopelman
-from matrix import Matrix
 import multiprocessing as mp
 from dijkstra import Dijkstra
 
 N = 50
-concentration = 60
 
 
 def find_shotest(matrix):
@@ -16,7 +13,8 @@ def find_shotest(matrix):
     minLength = 99999999999
     for i in range(len(matrix[0])):
         for j in range(len(matrix[0])):
-            path, cost = Dijkstra(matrix, (0, 1), (len(matrix) - 1, j))
+            d = Dijkstra(matrix, (0, 1), (len(matrix) - 1, j))
+            path, cost = d.method()
             if cost < minLength:
                 res = path
                 minLength = cost
@@ -42,22 +40,21 @@ def full_statistics_for_N():
 
     start_time = time()
     with mp.Pool(24) as p:
-        answer = np.array(p.map(statistics, list(np.arange(10)))).T
-        for i in range(len(answer)):
-            ind = round((i + 1) * 0.05, 2)
-            full_stat[ind] = answer[i]
-
-    df_full_stat = pd.DataFrame(full_stat)
-    df_full_stat.to_excel(f'./percolation_{N}_con_{concentration}.xlsx')
-    print('\nSequential execution time : %3.2f s.' % (time() - start_time))
+        for concentration in range(5, 100, 5):
+            answer = np.array(p.map(statistics, list(np.column_stack((np.full(1000, concentration), np.arange(1000)))))).T
+            full_stat['red'] = answer[0]
+            full_stat['black'] = answer[1]
+            df_full_stat = pd.DataFrame(full_stat)
+            df_full_stat.to_excel(f'./percolation_{N}_con_{concentration}.xlsx')
+            print('\nSequential execution time : %3.2f s.' % (time() - start_time))
 
 
 def statistics(i):
     stat = {}
-    red, black = count_cells(N, concentration)
+    red, black = count_cells(N, i[0])
     stat['red'] = red
     stat['black'] = black
-    print("Success ", i)
+    print("Success ", i[1])
     return list(stat.values())
 
 
